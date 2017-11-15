a36b01a76bc4fcf601c01924cffce85b33739058
walaa111 <walaagamil18@gmail.com>
2017-11-15 18:45:59 +0200

    a

--- lasttest/platforms/android/assets/www/cordova_plugins.js
+++ lasttest/platforms/android/assets/www/cordova_plugins.js
@@ -1,18 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "id": "cordova-plugin-firebase.FirebasePlugin",
-        "file": "plugins/cordova-plugin-firebase/www/firebase.js",
-        "pluginId": "cordova-plugin-firebase",
-        "clobbers": [
-            "FirebasePlugin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-firebase": "0.1.24",
     "cordova-android-play-services-gradle-release": "1.1.4"
 };
 // BOTTOM OF METADATA
