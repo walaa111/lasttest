e7a1f10963043369c44abfef5c94b6d3c30f7b96
walaa111 <walaagamil18@gmail.com>
2017-11-15 18:56:19 +0200

    q

--- lasttest/platforms/android/assets/www/cordova_plugins.js
+++ lasttest/platforms/android/assets/www/cordova_plugins.js
@@ -3,7 +3,8 @@ module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-android-play-services-gradle-release": "1.1.4"
+    "cordova-android-play-services-gradle-release": "1.1.4",
+    "cordova-android-support-gradle-release": "1.1.5"
 };
 // BOTTOM OF METADATA
 });
